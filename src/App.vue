5543e3b6f85991eb579afd9977488c6d4325b2e2
keside <wilsonezeala@yahoo.com>
2022-11-25 14:19:59 +0100

    add design tokens and font

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   export default {};
 </script>
 <template>
-  <div class="bg-red-400">project started</div>
+  <div class="dark" data-mode="dark">
+    <div class="primary-bg font-nunito-sans min-h-screen p-">
+      <h1 class="primary-text">project started</h1>
+    </div>
+  </div>
 </template>
 <style scoped></style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,vue}'],
+  darkMode: ['class', '[data-mode="dark"]'],
+
   theme: {
-    extend: {},
+    colors: {
+      dark: {
+        100: 'hsl(209, 23%, 22%)',
+        200: 'hsl(207, 26%, 17%)',
+        300: 'hsl(200, 15%, 8%)',
+      },
+
+      light: {
+        100: 'hsl(0, 0%, 100%)',
+        200: 'hsl(0, 0%, 98%)',
+        300: ' hsl(0, 0%, 52%)',
+      },
+    },
+    extend: {
+      fontFamily: {
+        'nunito-sans': ['Nunito Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
